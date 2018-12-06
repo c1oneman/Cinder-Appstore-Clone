eb9456e2d831be3edbee42de7ea627e25eb6b160
Clay Loneman <clay@claytons-macbook-pro.local>
2018-12-06 10:16:49 -0600

    Initial Commit

---
+++ Cinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinder
+//
+//  Created by Clayton Loneman on 12/6/18.
+//  Copyright © 2018 Clayton Loneman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
